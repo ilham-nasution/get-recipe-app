b358bf2439cba2e6401a4bf94cb7c928a56f30f5
Ilham Rahman <ilhaamrahman@gmail.com>
2020-06-04 13:07:28 +0700

    add loading spinner

--- src/Components/Recipe.jsx
+++ src/Components/Recipe.jsx
@@ -1,38 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { RotateSpinner } from "react-spinners-kit";
 import axios from "axios";
 
 const Recipe = () => {
   const { id } = useParams();
+  const [isLoading, setIsLoading] = useState(false);
   const [recipe, setRecipe] = useState({
     extendedIngredients: [],
   });
 
   useEffect(() => {
     const fetchRecipe = async () => {
+      setIsLoading(true);
       const result = await axios(
         `https://api.spoonacular.com/recipes/${id}/information?apiKey=541436aa58c2400e94d3d976aeac8f84`
       );
-      debugger;
       setRecipe(result.data);
+      setIsLoading(false);
     };
     fetchRecipe();
   }, [id]);
 
   return (
-    <div className="row">
-      <div className="col">
-        <img src={recipe.image} alt="food" />
-        <h1>{recipe.title}</h1>
-        <h5>Ready in {recipe.readyInMinutes} minutes</h5>
-        <ul>
-          {recipe.extendedIngredients.map((ingredient) => (
-            <li key={ingredient.id}>{ingredient.original}</li>
-          ))}
-        </ul>
-        <h4>{recipe.instructions}</h4>
+    <React.Fragment>
+      <div className="row flex-center margin-top-large">
+        {isLoading ? <RotateSpinner size={100} color={"#534c5c"} /> : ""}
       </div>
-    </div>
+      <div className="row">
+        <div className="col">
+          <img src={recipe.image} alt="food" />
+          <h1>{recipe.title}</h1>
+          <h5>Ready in {recipe.readyInMinutes} minutes</h5>
+          <ul>
+            {recipe.extendedIngredients.map((ingredient) => (
+              <li key={ingredient.id}>{ingredient.original}</li>
+            ))}
+          </ul>
+          <h4>{recipe.instructions}</h4>
+        </div>
+      </div>
+    </React.Fragment>
   );
 };
 
--- src/Components/RecipeList.jsx
+++ src/Components/RecipeList.jsx
@@ -1,37 +1,46 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { RotateSpinner } from "react-spinners-kit";
 import axios from "axios";
 
 const Homepage = () => {
   const { ingredient } = useParams();
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       const result = await axios(
         `https://api.spoonacular.com/recipes/findByIngredients?apiKey=541436aa58c2400e94d3d976aeac8f84&ingredients=${ingredient}&number=12`
       );
       setData(result.data);
+      setIsLoading(false);
     };
     fetchData();
   }, [ingredient]);
 
   return (
-    <div className="row flex-center child-borders">
-      {data.map((recipe) => (
-        <div key={recipe.id} className="col-3 col margin">
-          <img src={recipe.image} alt="food" />
+    <React.Fragment>
+      <div className="row flex-center margin-top-large">
+        {isLoading ? <RotateSpinner size={100} color={"#534c5c"} /> : ""}
+      </div>
+      <div className="row flex-center child-borders">
+        {data.map((recipe) => (
+          <div key={recipe.id} className="col-3 col margin">
+            <img src={recipe.image} alt="food" />
 
-          <div className="card-body">
-            <p className="card-title">{recipe.title}</p>
-            <Link to={`/recipes/detail/${recipe.id}`} className="btn-small">
-              Recipe detail
-            </Link>
+            <div className="card-body">
+              <p className="card-title">{recipe.title}</p>
+              <Link to={`/recipes/detail/${recipe.id}`} className="btn-small">
+                Recipe detail
+              </Link>
+            </div>
           </div>
-        </div>
-      ))}
-    </div>
+        ))}
+      </div>
+    </React.Fragment>
   );
 };
 
